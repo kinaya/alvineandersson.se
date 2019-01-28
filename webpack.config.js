1efef8f769988f008ed72515aa55f827518b1f37
Alvine <alvine@tamkin.se>
2019-01-28 19:18:49 +0100

    Removed old images

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-//  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
