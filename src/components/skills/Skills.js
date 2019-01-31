fdadcaddf6bee66dbd5a2ff2c08a4b0118692a4d
Alvine <alvine@tamkin.se>
2019-01-31 12:19:12 +0100

    whale

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -21,13 +21,15 @@ const Skills = ({}) => {
         </div>
 
         <div className="list">
-          <h4>Namedropping</h4>
+          <h4>Skillcloud</h4>
           <p><span className="label">Språk/Frameworks:</span> HTML, Css/Sass, PHP, JavaScript, JQuery, React/Redux, Node.js, Express.js</p>
-          <p><span className="label">CMS:</span> Drupal, Wordpress, SquareSpace</p>
-          <p><span className="label">Databaser:</span> MySQL, MariaDB, MongoDB</p>
-          <p><span className="label">Testning:</span> PHPUnit, Jest, Mocha</p>
-          <p><span className="label">Tekniker:</span> Service Workers, OAuth, JWT, Bootstrap, Materialize, SVG animation</p>
+          <p><span className="label">CMS:</span> Drupal, Wordpress</p>
+          <p><span className="label">Databaser:</span> MySQL/MariaDB, MongoDB</p>
+          <p><span className="label">Testning:</span> Jest, Mocha</p>
+          <p><span className="label">Tillgänglighet:</span> Accesibility guidelines, Wai-Aria</p>
+          <p><span className="label">APIs:</span> Facebook Api, Google Api</p>
           <p><span className="label">Utveckling och distribuering:</span> git, webpack, Apache/Cpanel, Heroku, Docker</p>
+          <p><span className="label">Annat:</span> SPA, Progressive Web apps, Service Workers, OAuth, JWT, Bootstrap, Materialize, SVG animation</p>
           <p>Grundläggande koll på Java/Android utveckling, samt Python. Gillar att bygga möjliga och omöjliga saker med Raspberry Pi eller Arduino, studerat media och kommunikation, ofta bollplank för koncept och ux i projekt, van projektledare och en stjärna på att googla.</p>
         </div>
 
