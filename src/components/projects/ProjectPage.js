308fea3248591bb4142c02207e456fc00cf0a971
Alvine <alvine@tamkin.se>
2019-02-06 16:36:32 +0100

    headlin adjustment

--- src/components/projects/ProjectPage.js
+++ src/components/projects/ProjectPage.js
@@ -30,7 +30,7 @@ const ProjectPage = ({project, projects}) => {
 
         <div className="inner page-header">
           <span className="case">Case story</span>
-          <h1 className="project-title">{project.title}</h1>
+          <h1 className={`project-title ${project.id}`}>{project.title}</h1>
           <p className="description">{project.info}</p>
         </div>
 
