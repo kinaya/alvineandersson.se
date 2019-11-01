5268d4109ebd925496ca265ba2b456d2950ef225
Alvine <alvine@tamkin.se>
2019-11-01 11:14:58 +0100

    new email adress

---
+++ README.md
@@ -0,0 +1,11 @@
+# Personal portfolio website for Alvine Andersson
+
+See it in action at https://alvineandersson.se
+
+## Tech
+
+Website built using React. Tests with Jest and Enzyme.
+
+## Want to work together?
+
+Contact me at hello@alvineandersson.se
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,8 +12,8 @@ const Footer = () => {
         <div className="contact">
           <p>Webbutvecklare med 10+ års erfarenhet. På plats eller distans, stora eller små projekt, front- eller backend.</p>
           <p>Hittas vid tangentbordet, på cykel genom Göteborgs gator eller i fjällen i en hemmabyggd husbil.</p>
-				  <a data-test="footer-link" href="mailto:hej@alvineandersson.se"><span>hej@alvineandersson.se</span></a>
-          <a data-test="footer-link" href="https://github.com/kinaya"><span>kinaya@github</span></a>
+				  <a data-test="footer-link" href="mailto:hej@alvineandersson.se"><span>hello@alvineandersson.se</span></a>
+          <a data-test="footer-link" href="https://github.com/kinaya"><span>github.com/kinaya</span></a>
           <a data-test="footer-link" href="https://tamkin.se"><span>tamkin.se</span></a>
         </div>
 
